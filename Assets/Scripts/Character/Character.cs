c53c3731a0ad62b6ee2e70a890d9bb8c1dd5fd2e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-22 07:04:13 +0900

    キャラクタとキャラクタコンポーネントそれぞれの基底クラス

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDev.Character
+{
+    /// <summary>
+    /// キャラクタクラス
+    /// </summary>
+    public abstract class Character : MonoBehaviour
+    {
+        /// <summary>
+        /// Componentリスト
+        /// </summary>
+        private List<CharacterComponent> Components = new List<CharacterComponent>();
+
+        /// <summary>
+        /// キャラクタコンポーネント追加
+        /// </summary>
+        /// <param name="Component">キャラクタコンポーネント</param>
+        protected void AddCharacterComponent(CharacterComponent Component)
+        {
+            Components.Add(Component);
+        }
+
+        void Update()
+        {
+            foreach (var Component in Components)
+            {
+                Component.OnUpdate();
+            }
+        }
+
+        void FixedUPdate()
+        {
+            foreach (var Component in Components)
+            {
+                Component.OnFixedUpdate();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Character/CharacterComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDev.Character
+{
+    /// <summary>
+    /// キャラクタコンポーネント基底クラス
+    /// </summary>
+    public abstract class CharacterComponent
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public CharacterComponent()
+        {
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        public virtual void OnUpdate() { }
+
+        /// <summary>
+        /// FixedUpdate
+        /// </summary>
+        public virtual void OnFixedUpdate() { }
+    }
+}
